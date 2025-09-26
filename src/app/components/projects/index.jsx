88d3db142362bdde17f38e92c2da0ad9ef2cfdc5
Zvezda Neycheva <138030249+ZvezdaNeycheva@users.noreply.github.com>
2025-09-26 19:21:21 +0300

    Update index.jsx

--- src/app/components/projects/index.jsx
+++ src/app/components/projects/index.jsx
@@ -22,7 +22,7 @@ const ProjectList = ({ projects }) => {
             animate='show'
             className="flex flex-col items-center justify-between">
             <h1 className="text-4xl font-bold mb-4 -mt-16">Projects</h1>
-            <div className="w-full max-w-auto xl:max-w-4xl px-4 mx-auto lg:px-16 space-y-6 md:space-y-8 flex flex-col items-center">
+            <div className="w-full max-w-auto xl:max-w-7xl px-4 mx-auto lg:px-16 space-y-6 md:space-y-8 flex flex-col items-center">
                 {projects.map((project) => (
                     <ProjectLayout key={project.id} {...project} />
                 ))}
