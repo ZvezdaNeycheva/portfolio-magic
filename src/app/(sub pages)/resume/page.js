c2d5b2739d996d1363a21e21e1b7360b7549b4e1
Zvezda Neycheva <138030249+ZvezdaNeycheva@users.noreply.github.com>
2026-05-21 16:36:20 +0000

    window/self/document (browser APIs) during server-side rendering (SSR) on Vercel make vercel build crash

--- src/app/(sub pages)/resume/page.js
+++ src/app/(sub pages)/resume/page.js
@@ -1,5 +1,8 @@
-'use client'
-import ResumeDetails from "@/components/resume";
+import dynamic from 'next/dynamic';
+const ResumeDetails = dynamic(
+  () => import("@/components/resume"),
+  { ssr: false }
+);
 
 export default function Home() {
     return (
